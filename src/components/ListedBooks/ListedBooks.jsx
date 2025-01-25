eec5f526fe3acba7f1f2af01db512294adbd0afc
jobayer58 <jobayerafnan770@gmail.com>
2025-01-25 23:08:01 +0600

    update

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -7,6 +7,7 @@ import Book from '../Book/Book';
 
 const ListedBooks = () => {
     const [readList, setReadList] = useState([])
+    const [sort, setSort] = useState('')
     const allBooks = useLoaderData()
 
     useEffect(() => {
@@ -18,9 +19,30 @@ const ListedBooks = () => {
 
     }, [])
 
+    const handleSort = sortType => {
+        setSort(sortType)
+
+        if (sortType === 'num of pages') {
+            const sortReadList = [...readList].sort((a, b) => a.totalPages - b.totalPages)
+            setReadList(sortReadList)
+        }
+
+        if (sortType === 'Ratings') {
+            const sortReadList = [...readList].sort((a,b) => a.rating - b.rating)
+            setReadList(sortReadList)
+        }
+    }
+
     return (
         <div>
             <h3 className='text-3xl my-8'>listed books</h3>
+            <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn m-1">{sort ? `sort by:${sort}` : 'Sort By'}</div>
+                <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                    <li onClick={() => handleSort('Ratings')}><a>Rating</a></li>
+                    <li onClick={() => handleSort('num of pages')}><a>No of pages</a></li>
+                </ul>
+            </div>
             <Tabs>
                 <TabList>
                     <Tab>Read List</Tab>
